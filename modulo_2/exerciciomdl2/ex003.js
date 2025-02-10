ea45af671c306d548e7aa6a4b4fc4c1fc10c2597
suricatstar <caue.tamiarana@gmail.com>
2025-02-09 21:27:24 -0300

    tudo

--- modulo_2/exerciciomdl2/ex003.js
+++
@@ -1,7 +0,0 @@
-const carros = require('./ex001');
-
-carros.push({ placa: "DFC3R43", modelo: "t-cross", ano: 2022 });
-
-carros.sort((a, b) => a.modelo.localeCompare(b.modelo));
-
-module.exports = carros;
