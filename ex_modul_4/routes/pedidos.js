7ce72959267d91ea74b1be265f8fb0cc56389a8f
Cauê Tamiarana <caue.tamiarana@gmail.com>
2025-02-23 19:31:13 -0300

    finalizado exercicio

--- ex_modul_4/routes/pedidos.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.render("pedidos/index", { nomePizzaria: "Pizzaria do Zé" });
-});
-
-module.exports = router;
--- ex_modul_4/routes/perfil.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('perfil/index', {perfil: 'Cauê Tamiarana', idade: 21});
-});
-
-module.exports = router;
